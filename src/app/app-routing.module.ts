e4fc52fb70f07fb59a44f4796f4d383d4cd9d281
jmneko <new-neko@hotmail.com>
2023-08-06 23:50:16 +0200

    correccion nombres de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FormComponent } from './componets/form/form.component';
-import { ViewPostComponent } from './pages/view-post/view-post.component';
+import { ViewUserComponent } from './pages/view-user/view-user.component';
 import { C404Component } from './pages/c404/c404.component';
 
 const routes: Routes = [
   {path:"",pathMatch: 'full', redirectTo: 'home'},
   {path: "home", component: HomeComponent},
-  {path:"newpost", component: FormComponent},
-  {path: "updatepost", component: FormComponent},
-  {path: "viewpost/:idpost", component: ViewPostComponent},
+  {path:"newuser", component: FormComponent},
+  {path: "updateuser/:iduser", component: FormComponent},
+  {path: "viewuser/:iduser", component: ViewUserComponent},
   {path: "**", component: C404Component}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ViewPostComponent } from './pages/view-post/view-post.component';
+import { ViewUserComponent } from './pages/view-user/view-user.component';
 import { FormComponent } from './componets/form/form.component';
-import { PostCardComponent } from './componets/post-card/post-card.component';
+import { UserCardComponent } from './componets/user-card/user-card.component';
 import { C404Component } from './pages/c404/c404.component';
+import { NavBarComponent } from './componets/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ViewPostComponent,
+    ViewUserComponent,
     FormComponent,
-    PostCardComponent,
-    C404Component
+    UserCardComponent,
+    C404Component,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
