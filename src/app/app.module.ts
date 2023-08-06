72af7d33bc0efebebcbb3646ca438393966ff4c9
jmneko <new-neko@hotmail.com>
2023-08-07 01:47:48 +0200

    llamando a API y creando getAll y getById

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/Http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { NavBarComponent } from './componets/nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/Http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, lastValueFrom } from 'rxjs';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
+  httpClient = inject(HttpClient);
+  private baseUrl: string = "https://peticiones.online/api/users/";
+
   constructor() { }
+
+
+  getAll(): Promise<any[]> {
+
+
+    return lastValueFrom(this.httpClient.get<any[]>(this.baseUrl))
+  }
+
+
+
+  getById(_id: string): Observable<User> {
+    return this.httpClient.get<any>(`${this.baseUrl}${_id}`)
+  }
+
+
 }
