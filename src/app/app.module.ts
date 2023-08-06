85c5885fe466b8dc340bdba693a9913ebadd58af
jmneko <new-neko@hotmail.com>
2023-08-06 22:11:35 +0200

    Creacion de componentes, interfaz y servicios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ViewPostComponent } from './pages/view-post/view-post.component';
+import { FormComponent } from './componets/form/form.component';
+import { PostCardComponent } from './componets/post-card/post-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewPostComponent,
+    FormComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
